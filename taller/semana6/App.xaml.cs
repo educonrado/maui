2930826d5f6d9718eb564e35760f04b3a3cdd429
Edu Conrado <excz.test@gmail.com>
2025-05-20 19:44:29 -0500

    Update App.xaml.cs

--- taller/semana6/App.xaml.cs
+++ taller/semana6/App.xaml.cs
@@ -9,7 +9,7 @@
 
         protected override Window CreateWindow(IActivationState? activationState)
         {
-            return new Window(new Views.Usuarios());
+            return new Window(new NavigationPage(new Views.Usuarios()));
         }
     }
 }
